de20ba711e4ec4493eaa635f447f0943591a2214
AxelLaisney <axel.laisney@hotmail.com>
2025-03-11 15:07:39 +0100

    d

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 require("dotenv").config()
+require("./src/models/models")
 
 const express = require("express")
 const bodyParser = require("body-parser")
-fhsdkfkjsdhfkj
+
 const app = express()
 
 const PORT = process.env.SERVER_PORT || 3000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extends: false}))
+
+app.use( require("./src/routers/biereRouter"))
 
 app.listen(PORT, () => {
     console.log("server listening on port ", PORT)
