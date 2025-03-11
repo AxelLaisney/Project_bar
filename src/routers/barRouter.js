ccb9f5a4973fa5a6f4463726526fe6b2445d91d2
Cyril29D <cyrildelly9@gmail.com>
2025-03-11 15:04:44 +0100

    mise en place de bar

--- src/routers/barRouter.js
+++ src/routers/barRouter.js
@@ -1,8 +1,16 @@
 const Router = require("express").Router()
 
-const { index, show, update, create, destroy} = require("../../controller/barController")
+const { index, show, update, destroy, store} = require("../controller/barController")
+
+
+Router.get("/bars", index)
+
+Router.get("/bars/:id", show)
+
+Router.post("/bars", store)
+
+Router.put("/bars/:id", update)
 
 Router.delete("/bars/:id", destroy);
-
 
 module.exports = Router;
