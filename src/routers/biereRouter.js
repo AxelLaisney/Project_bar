5eeb9f2ec1a7ed095bd993769786f03d6b2e8ec2
AxelLaisney <axel.laisney@hotmail.com>
2025-03-11 15:01:36 +0100

    Basic CRUD for biere

--- src/routers/biereRouter.js
+++ src/routers/biereRouter.js
@@ -1,11 +1,15 @@
-const Router = require("express").Router()
+const router = require("express").Router()
 
-const { index, show, update, create, destroy} = require("../../controller/biereController")
+const { index, show, update, create, destroy} = require("../controllers/bierreController")
+
+router.get("/bars/:id_bar/biere", index)
+router.get("/biere/:id", show)
+
+router.post("/bars/:id_bar/biere", create)
+
+router.put("/biere/:id", update)
+
+router.delete("/biere/:id", destroy)
 
 
-
-
-Router.delete("/biere/:id", destroy);
-
-
-module.exports = Router;
+module.exports = router;
