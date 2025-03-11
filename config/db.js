df4e8983fef782cf0a12c0929700fc1e1d3376cf
AxelLaisney <axel.laisney@hotmail.com>
2025-03-11 11:39:31 +0100

    models added

--- config/db.js
+++ config/db.js
@@ -1,6 +1,13 @@
 const Sequelize = require("sequelize")
 
 const Dialect = process.env.DIALECT || "sqlite"
-const Storage = process.env.DB || "./db.sqlite"
+const Storage = process.env.DB || "db.sqlite"
 
-const db = new
+const db = new Sequelize({
+    dialect: Dialect,
+    storage: Storage
+})
+
+db.sync()
+
+module.exports = db
---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config()
+
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express()
+
+const PORT = process.env.SERVER_PORT || 3000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extends: false}))
+
+app.listen(PORT, () => {
+    console.log("server listening on port ", PORT)
+})
+
